9ccd40b73db69de988d1ad372f20080ab00d9a7b
Nicolás Carreño <n.carrenoh2019@gmail.com>
2020-04-11 15:04:56 -0400

    ref(src) delete

--- src/store/actions/app.js
+++
@@ -1,5 +0,0 @@
-export const updatePost = (type, data) => (dispatch, getState) => {
-  const { post } = getState().app;
-  post[type] = data;
-  dispatch({ type: 'UPDATE_POST', payload: post });
-};
--- src/store/actions/index.js
+++
@@ -1,8 +0,0 @@
-import * as user from './user';
-import * as posts from './post';
-import * as app from './app';
-export const actions = {
-  app,
-  user,
-  posts,
-};
