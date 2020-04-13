a487838f92f69608e04b23ab1adcaa2834032212
Nicolás Carreño <n.carrenoh2019@gmail.com>
2020-04-13 09:18:07 -0400

    deleted Pods files

--- ios/Pods/Target Support Files/Pods-reduxApp/Pods-reduxApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_reduxApp : NSObject
-@end
-@implementation PodsDummy_Pods_reduxApp
-@end
--- ios/Pods/Target Support Files/Pods-reduxAppTests/Pods-reduxAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_reduxAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_reduxAppTests
-@end
