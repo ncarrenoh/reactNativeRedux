1b7d64fed55139f4891085f4f3e7d07ccce53449
Nicolás Carreño <n.carrenoh2019@gmail.com>
2020-04-13 17:15:35 -0400

    before checkout

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import Post from './src/components/Post';
 import { store } from './src/store';
 import Navigator from './src/routes/app';
-import { AuthLoading } from './src/screens';
+import { AuthLoading, RegisterScreen } from './src/screens';
 
 console.disableYellowBox = true;
 // export default () => {
@@ -16,43 +16,45 @@ console.disableYellowBox = true;
 //   );
 // };
 
-export default () => {
-  const [users, setUsers] = useState([]);
-  const [refreshing, setRefreshing] = useState(true);
-  async function fetchUsers() {
-    const res = await axios.get('https://api.randomuser.me/?results=30');
-    const data = await res.data.results;
-    if (data.length) {
-      setUsers(data);
-      setRefreshing(false);
-    } else {
-      console.log('err');
-    }
-  }
+export default () => <RegisterScreen />;
+// POST COMPONENT
+// export default () => {
+//   const [users, setUsers] = useState([]);
+//   const [refreshing, setRefreshing] = useState(true);
+//   async function fetchUsers() {
+//     const res = await axios.get('https://api.randomuser.me/?results=30');
+//     const data = await res.data.results;
+//     if (data.length) {
+//       setUsers(data);
+//       setRefreshing(false);
+//     } else {
+//       console.log('err');
+//     }
+//   }
 
-  useEffect(() => {
-    fetchUsers();
-  }, []); // componentDidMount
+//   useEffect(() => {
+//     fetchUsers();
+//   }, []); // componentDidMount
 
-  function renderPost({ item }) {
-    return <Post user={item} />;
-  }
+//   function renderPost({ item }) {
+//     return <Post user={item} />;
+//   }
 
-  const handleOnRefresh = useCallback(() => {
-    setRefreshing(true);
-    setTimeout(() => fetchUsers());
-  });
+//   const handleOnRefresh = useCallback(() => {
+//     setRefreshing(true);
+//     setTimeout(() => fetchUsers());
+//   });
 
-  return users.length ? (
-    <FlatList
-      keyExtractor={item => item.id.value}
-      initialNumToRender={4}
-      data={users}
-      renderItem={renderPost}
-      onRefresh={handleOnRefresh}
-      refreshing={refreshing}
-    />
-  ) : (
-    <ActivityIndicator size="large" />
-  );
-};
+//   return users.length ? (
+//     <FlatList
+//       keyExtractor={item => item.id.value}
+//       initialNumToRender={4}
+//       data={users}
+//       renderItem={renderPost}
+//       onRefresh={handleOnRefresh}
+//       refreshing={refreshing}
+//     />
+//   ) : (
+//     <ActivityIndicator size="large" />
+//   );
+// };
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -5,41 +5,75 @@ const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
-    width,
+    maxWidth: width,
     maxHeight: height,
     flex: 1,
     justifyContent: 'center',
-    padding: 20,
+    alignItems: 'center',
+    padding: 10,
     margin: 10,
   },
   inputForm: {
-    width,
+    width: width * 0.9,
     height: 50,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    paddingLeft: 10,
+    paddingBottom: 5,
+    marginBottom: 10,
+  },
+  form: {
+    width,
+    marginHorizontal: 40,
+
+    maxHeight: height * 0.6,
+
+    backgroundColor: 'red',
   },
 });
 
-function InputForm({ placeholder, onChange, value }) {
+function InputForm({ placeholder, onChangeText, value }) {
   return (
     <TextInput
+      keyboardAppearance="dark"
       style={styles.inputForm}
       placeholder={placeholder}
-      onChange={onChange}
+      autoCapitalize="none"
       value={value}
+      onChangeText={v => onChangeText(v)}
     />
   );
 }
 
+function Form({ children }) {
+  return <View style={styles.form}>{children}</View>;
+}
+
 export default function Register(props) {
   const [email, setEmail] = useState('');
+  // const [name, setName] = useState('');
 
-  const emailOnChange = ({ item }) => {
+  const emailOnChange = item => {
     setEmail(item);
   };
+  // const nameOnChange = item => {
+  //   setName(item);
+  // };
   return (
     <View style={styles.container}>
-      <InputForm placeholder="Email" onChange={emailOnChange} value={email} />
-      {/* <InputForm placeholder="FullName" onChange={nameOnChange}/>
-      <InputForm placeholder="Username" onChange={usernameOnChange}/>
+      <Form>
+        <InputForm
+          placeholder="Ingrese su email"
+          onChangeText={emailOnChange}
+          value={email}
+        />
+      </Form>
+      {/* <InputForm
+        placeholder="Ingrese su nombre completo"
+        onChangeText={nameOnChange}
+        value={name}
+      /> */}
+      {/* <InputForm placeholder="Username" onChange={usernameOnChange}/>
       <InputForm placeholder="Password" onChange={passOnChange}/> */}
     </View>
   );
